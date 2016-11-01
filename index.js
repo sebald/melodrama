120efe56f5d1c007c9ed79549527b2be7f6ad248
Sebastian Sebald <sebastian.sebald@gmail.com>
2016-11-01 23:06:22 +0100

    :package: Use `execa` over `cross-spawn`.
    ...even though it is a dependencies :-x

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const chalk = require('chalk');
 const commandExists = require('command-exists');
 const fs = require('fs-extra');
 const meow = require('meow');
-const spawn = require('cross-spawn');
+const spawn = require('execa');
+
 
 // Helper to make sure we're not overwriting things
 const allowedFiles = ['.DS_Store', 'Thumbs.db', '.git', '.gitignore', '.vscode', 'README.md', 'LICENSE'];
 const noFileConflict = dir => {
   return fs.readdirSync(dir)
     .every(file => allowedFiles.indexOf(file) >= 0);
-}
+};
+
+// Helper to check if we can use yarn.
+const isYarnAvailable = () => new Promise((resolve, reject) => {
+  commandExists('yarn', (err, isAvailable) => {
+    if (err) { reject(err); }
+    resolve(isAvailable);
+  });
+});
 
 
 const cli = meow(`
@@ -60,44 +69,51 @@ fs.writeFileSync(
   JSON.stringify(pkg, null, 2)
 );
 
-// Do NOT install dependencies (for testing)
+// Do NOT install dependencies (for testing only!)
 if (cli.flags.skipInstall) {
   console.log(chalk.yellow('Skipping installation of dependencies.'));
   process.exit();
 }
 
-// Can we use `yarn`?
-commandExists('yarn', (err, cmdExists) => {
-  let cmd, args;
-  if (cmdExists) {
-    cmd = 'yarn';
-    args = ['add', '--exact'];
-  } else {
-    cmd = 'npm';
-    args = ['install', '--SE'];
-  }
-  if (cli.flags.verbose) { args.push('--verbose'); }
-
-  // Add dependencies
-  args.push(
-    'react',
-    'react-dom',
-    'spectacle'
-  );
-  if(cli.flags.syntax) {
-    args.push('prismjs');
-  }
-
-  // Go and install everything.
-  console.log(chalk.cyan('Installing dependencies. This may take a while...'));
-  console.log();
-  process.chdir(rootDir);
-  let proc = spawn(cmd, args, {stdio: 'inherit'});
-  proc.on('close', code => {
-    if (code !== 0) {
-      console.log(chalk.red(`Execution of command ${cmd} ${args.join(' ')} failed!`));
-      return;
+// Install dependencies
+isYarnAvailable()
+  .then(isAvailable => {
+    let cmd, args;
+    if (isAvailable) {
+      cmd = 'yarn';
+      args = ['add', '--exact'];
+    } else {
+      cmd = 'npm';
+      args = ['install', '--SE'];
     }
+    if (cli.flags.verbose) { args.push('--verbose'); }
+
+    // Add dependencies
+    args.push(
+      'react',
+      'react-dom',
+      'spectacle'
+    );
+    if(cli.flags.syntax) {
+      args.push('prismjs');
+    }
+
+    // Go and install everything.
+    console.log(chalk.cyan('Installing dependencies. This may take a while...'));
+    console.log();
+    process.chdir(rootDir);
+
+    const child = spawn(cmd, args);
+    if(cli.flags.verbose) {
+      child.stdout.pipe(process.stdout);
+    }
+    return child;
+  })
+  .then(() => {
     console.log('<insert init script here>');
+  })
+  .catch(err => {
+    console.log(chalk.red('Installation failed because of the following reaons:'));
+    console.log(chalk.red(err));
+    process.exit(1);
   });
-});
