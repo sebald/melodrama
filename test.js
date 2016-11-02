a002491c08e5fe7039beae1b86a9156a1c787556
Sebastian Sebald <sebastian.sebald@gmail.com>
2016-11-02 20:02:53 +0100

    :package: Promises, prompts + theme suggestions
    - Use promises instead of callbacks.
    - Use prompts instead of CLI flags
    - Query npm registry for theme suggestions.

--- test.js
+++ test.js
@@ -1,45 +1,67 @@
 import test from 'ava';
-import spawn from 'execa';
 import path from 'path';
 import fs from 'fs-extra';
 import { v4 } from 'uuid';
+import {
+  run,
+  prepareDirectory,
+  prepareInstallCommand,
+  prepareDependencies } from './init';
+
 
 const tmpDir = path.resolve(process.cwd(), '.tmp');
 const getTmpDir = () => path.resolve(tmpDir, v4());
-
-const index = require.resolve('./index');
-async function melodrama (...args) {
-  args.unshift(index);
-  args.push('--skip-install');
-  return spawn('node', args);
-}
 
 test.before('clean up temporary directory', () => {
   fs.removeSync(tmpDir);
 });
 
-test('show help if no <dir>', async t => {
-  const result = await melodrama();
-  t.is(typeof result.stdout, 'string');
+// Just a smoke test...
+test('expose a `run` command', async t => {
+  t.is(typeof run, 'function');
 });
 
-test('can be added to existing direcotry', async t => {
-  const dir = getTmpDir();
-  fs.mkdirsSync(dir);
-  const result = await melodrama(dir);
-  t.is(result.code, 0);
-});
-
-test('does abort if there folder has some non-allowed files', async t => {
-  t.throws(melodrama('.'));
-});
-
-test('create package.json in <dir>', async t => {
-  const dir = getTmpDir();
-  const result = await melodrama(dir);
+test('prepare directory', async t => {
+  const dir = await prepareDirectory(getTmpDir());
   const pkg = fs.readJsonSync(`${dir}/package.json`);
 
-  t.is(result.code, 0);
-  t.is(typeof pkg.name, 'string');
-  t.is(pkg.version, '1.0.0');
+  t.true(fs.existsSync(dir));
+  t.is(pkg.name, path.basename(dir));
+  t.is(typeof pkg.version, 'string');
+});
+
+test('prepare install command', async t => {
+  const {cmd, args} = await prepareInstallCommand();
+
+  t.regex(cmd, /yarn|npm/);
+  t.regex(args[0], /add|install/);
+  t.regex(args[1], /--exact|--SE/);
+});
+
+test('prepare dependencies (no syntax)', t => {
+  let dependencies = prepareDependencies({ syntax: false });
+
+  t.true(dependencies.indexOf('react') >= 0);
+  t.true(dependencies.indexOf('react-dom') >= 0);
+  t.true(dependencies.indexOf('spectacle') >= 0);
+  t.false(dependencies.indexOf('prismjs') >= 0);
+});
+
+test('prepare dependencies (with syntax)', t => {
+  let dependencies = prepareDependencies({ syntax: true });
+
+  t.true(dependencies.indexOf('react') >= 0);
+  t.true(dependencies.indexOf('react-dom') >= 0);
+  t.true(dependencies.indexOf('spectacle') >= 0);
+  t.true(dependencies.indexOf('prismjs') >= 0);
+});
+
+test('prepare dependencies (with theme)', t => {
+  let dependencies = prepareDependencies({ syntax: true, theme: 'foo' });
+
+  t.true(dependencies.indexOf('react') >= 0);
+  t.true(dependencies.indexOf('react-dom') >= 0);
+  t.true(dependencies.indexOf('spectacle') >= 0);
+  t.true(dependencies.indexOf('prismjs') >= 0);
+  t.true(dependencies.indexOf('spectacle-theme-foo') >= 0);
 });
